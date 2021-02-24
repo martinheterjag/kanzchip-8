d2132190e7b849e0ecf0102e8723c54b748e6b30
martinheterjag <martine7@kth.se>
2021-02-25 00:47:51 +0100

    Add logger module (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+// Copyright 2021 kanzchip-8 Project Authors. Licensed under MIT License.
+
+#[macro_use]
+mod logger;
+
 fn main() {
     println!("Print hex values! 0x{:0>4X} and 0x{:0>4x}", 10, 512);
+
+    log_error!("HELLO!");
+    log_warning!("Hello World");
+    log_info!("Hello World...");
+    log_debug!("Hello World.....");
 }
