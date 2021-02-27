c209e8de3e87ada328c19be6a2055f1134501ba8
martinheterjag <martine7@kth.se>
2021-02-28 00:16:26 +0100

    Add memory class with a few regísters

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 import time
 from log import logger
 from screen import Screen
+from memory import Memory
 
 VERSION = "0.0.1"
 
@@ -10,7 +11,12 @@ VERSION = "0.0.1"
 if __name__ == "__main__":
     logger.info("kanzchip-8, chip-8 emulator version " + VERSION + "-")
     screen = Screen()
+    memory = Memory()
     # TODO: Replace test code below
+    logger.debug("V4 is set to: 0x{:X}".format(memory.get_vx(4)))
+    memory.set_vx(4, 0xFF)
+    logger.debug("V4 is set to: 0x{:X}".format(memory.get_vx(4)))
+
     x = 0
     y = 0
     count = 0
---
+++ src/memory.py
@@ -0,0 +1,55 @@
+# Copyright authors of kanzchip-8, licenced under MIT licence
+
+from log import logger
+
+class Memory:
+    def __init__(self):
+        # Allocate memory for all registers
+        self.reg_v = [0] * 16     # Vx where x is 0-15, 8.bit registers
+        self.reg_i = 0            # 16-bit register
+        self.reg_vf = 0           # Flag register can be 1 or 0
+        self.reg_delay = 0        # 8-bit timer
+        self.reg_sound = 0        # 8-bit timer
+        self.program_counder = 0  # 16-bit PC
+        self.stack = [0] * 16     # 16-bit array
+        self.stack_pointer = 0    # 8-bit
+        logger.info("Registers initialized")
+
+    def is_16_bit(self, value):
+        return (value > 0 and value < 0x10000)
+
+    def is_8_bit(self, value):
+        print ("value {}".format((value > 0 and value < 0x100)))
+        return (value > 0 and value < 0x100)
+
+    def set_vx(self, vx, value):
+        assert self.is_8_bit(value)
+        self.reg_v[vx] = value
+
+    def get_vx(self, vx):
+        return self.reg_v[vx]
+
+    def set_i(self, value):
+        assert self.is_16_bit(value)
+        self.reg_i = value
+
+    def get_i(self):
+        return self.reg_i
+
+    def set_vf(self, flag):
+        assert flag == 0 or flag == 1
+        self.reg_vf = flag
+
+    def get_pc(self):
+        return self.program_counder
+
+    def set_pc(self, value):
+        assert self.is_16_bit(value)
+        self.program_counder = value
+
+    def incr_pc(self):
+        self.program_counder = self.program_counder + 1
+        # Emulate 16 bit register rollover
+        if self.program_counder > 0xFFFF:
+            logger.warning("program counter rollover")
+            self.program_counder = 0
