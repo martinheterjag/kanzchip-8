213126cec1331631513b0c4f0ddf33b3042d551c
Chris Stewart <cstewart90@users.noreply.github.com>
2021-02-28 17:44:04 +0000

    Removed global variable from log.py as it is not necessary.

--- src/log.py
+++ src/log.py
@@ -1,18 +1,18 @@
 import logging
 
-global logger
 
 def setup_custom_logger(name):
-    FORMAT = ("%(asctime)-s %(levelname)s %(filename)s " +
-             "%(funcName)s %(lineno)d: %(message)s")
+    FORMAT = ("%(asctime)s - %(levelname)s %(filename)s " +
+              "%(funcName)s %(lineno)d: %(message)s")
     formatter = logging.Formatter(fmt=FORMAT)
 
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
 
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
-    return logger
+    custom_logger = logging.getLogger(name)
+    custom_logger.setLevel(logging.DEBUG)
+    custom_logger.addHandler(handler)
+    return custom_logger
+
 
 logger = setup_custom_logger('global')
