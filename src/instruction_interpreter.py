ce5aa870cca56b4eb672688c0ff04bb86569a5fb
martinheterjag <martine7@kth.se>
2021-03-02 11:24:25 +0100

    Implement function interpret_group_8()

--- src/instruction_interpreter.py
+++ src/instruction_interpreter.py
@@ -3,6 +3,7 @@
 from log import logger
 
 
+
 class InstructionInterpreter:
     def __init__(self, screen):
         # Allocate memory for all registers
@@ -17,6 +18,9 @@ class InstructionInterpreter:
         # 0x000 - 0xFFF (4095). 0x000-0x1FF is reserved.
         # Most programs start at 0x200
         self.memory = bytearray(4096)
+
+        # TODO: Decide if this should be a setting or not?
+        self.CHIP_48 = False
 
         self.screen = screen
         logger.info("InstructionInterpreter initialized")
@@ -37,6 +41,61 @@ class InstructionInterpreter:
             self.stack_pointer -= 1
         else:
             logger.warning("OpCode {:X} not supported!".format(instruction))
+
+    def interpret_group_8(self, instruction):
+        # Logic and arithmetic operations between Vx and Vy
+        x = (instruction & 0x0F00) >> 8
+        y = (instruction & 0x00F0) >> 4
+        last_nibble = instruction & 0x000F
+
+        logger.debug("instruction:{:X} x:{:X} y:{:X}".format(instruction, x, y))
+        if last_nibble == 0x0:  # LD Vx, Vy
+            self.reg_v[x] = self.reg_v[y]
+        elif last_nibble == 0x1:  # OR Vx, Vy
+            self.reg_v[x] = self.reg_v[x] | self.reg_v[y]
+        elif last_nibble == 0x2:  # AND Vx, Vy
+            self.reg_v[x] = self.reg_v[x] & self.reg_v[y]
+        elif last_nibble == 0x3:  # XOR Vx, Vy
+            self.reg_v[x] = self.reg_v[x] ^ self.reg_v[y]
+        elif last_nibble == 0x4:  # ADD Vx, Vy
+            # Vf is used as carry
+            if self.reg_v[x] + self.reg_v[y] > 0xFF:
+                self.reg_v[0xF] = 1
+            else:
+                 self.reg_v[0xF] = 0
+            self.reg_v[x] = (self.reg_v[x] + self.reg_v[y]) % 0x100
+        elif last_nibble == 0x5:  # SUB Vx, Vy
+            # If negative, wrap around and set Vf as carry
+            if reg_v[x] > reg_v[y]:
+                tmp = self.reg_v[x] - self.reg_v[y]
+                rev[x] = 0x100 - tmp
+                self.reg_v[0xF] = 1
+            else:
+                self.reg_v[x] = self.reg_v[x] - self.reg_v[y]
+                self.reg_v[0xF] = 0
+        elif last_nibble == 0x6:  # SHR Vx {, Vy}
+            # Vf shall be set to same value as the bit that is shifted out
+            if CHIP_48 == True:
+                self.reg_v[x] = self.reg_v[y]
+            self.reg_v[0xF] = self.reg_v[x] & 0x0001
+            self.reg_v[x] = self.reg_v[x] >> 1
+        elif last_nibble == 0x7:  # SUBN Vx, Vy
+            # If negative, wrap around and set Vf as carry
+            if reg_v[y] > reg_v[x]:
+                tmp = self.reg_v[y] - self.reg_v[x]
+                rev[x] = 0x100 - tmp
+                self.reg_v[0xF] = 1
+            else:
+                self.reg_v[x] = self.reg_v[y] - self.reg_v[x]
+                self.reg_v[0xF] = 0
+        elif last_nibble == 0xE:  # SHL Vx {, Vy}
+            # Vf shall be set to same value as the bit that is shifted out
+            if CHIP_48 == True:
+                self.reg_v[x] = self.reg_v[y]
+            self.reg_v[0xF] = self.reg_v[x] & 0x8000
+            self.reg_v[x] = self.reg_v[x] << 1
+        else:
+            logger.warning("OpCode {:X} supported ".format(instruction))
 
     def interpret_instruction(self, instruction):
         if instruction < 0x00:
@@ -75,7 +134,7 @@ class InstructionInterpreter:
             logger.warning("OpCode {:X} not yet supported ".format(instruction))
         elif instruction & 0xF000 == 0x8000:
             # Arithmetic (Vx + Vy, Vx xor Vy etc.)
-            logger.warning("OpCode {:X} not yet supported ".format(instruction))
+            self.interpret_group_8(instruction)
         elif instruction & 0xF000 == 0x9000:
             # SNE
             logger.warning("OpCode {:X} not yet supported ".format(instruction))
--- unit-test/test_instructions.py
+++ unit-test/test_instructions.py
@@ -12,5 +12,21 @@ class TestInstructions(unittest.TestCase):
         ii.interpret_instruction(0x00E0)
         screen.clear_all.assert_called_once()
 
+    # Test is supposed to verify that 8xy0 load ii.reg_v[y] into ii.reg_v[x]
+    def test_8xy0_load_vx_vy(self):
+        screen = Mock()
+        ii = InstructionInterpreter(screen)
+        ii.reg_v[0x0] = 0x10
+        ii.reg_v[0x1] = 0x11
+        ii.reg_v[0xA] = 0x12
+        # Load V1 == 0x11 into V0
+        self.assertEqual(ii.reg_v[0x0], 0x10)
+        ii.interpret_instruction(0x8010)
+        self.assertEqual(ii.reg_v[0x0], 0x11)
+        # Load VA == 0x12 into V1
+        self.assertEqual(ii.reg_v[0x1], 0x11)
+        ii.interpret_instruction(0x81A0)
+        self.assertEqual(ii.reg_v[0xA], 0x12)
+
 if __name__ == '__main__':
     unittest.main()
