8915c9108b78a43efb66e745d651496dc766f545
Chris Stewart <cstewart90@users.noreply.github.com>
2021-03-09 21:52:02 +0000

    Set mainloop fps_limit to 0, since clock.tick() is already enforcing 60 fps and default for mainloop was 30. - Removed extra spin() - Removed params from reset_rom and open_rom_file, since they have access   to ii and screen now - Formatting

--- src/__main__.py
+++ src/__main__.py
@@ -18,11 +18,12 @@ VERSION = "0.0.1"
 def main():
     logger.info(f"--- kanzchip-8, chip-8 emulator version {VERSION} ---")
     title = ""
-    def reset_rom(instruction_interpreter, screen):
-        screen.clear_all()
-        instruction_interpreter.reset()
 
-    def open_rom_file(instruction_interpreter, screen):
+    def reset_rom():
+        screen.clear_all()
+        ii.reset()
+
+    def open_rom_file():
         root = tk.Tk()
         root.withdraw()
         rom = filedialog.askopenfilename(initialdir="roms",
@@ -32,11 +33,11 @@ def main():
             logger.info("No rom selected!")
             logger.info("Exiting")
             quit()
-        instruction_interpreter.load_rom(rom)
+        ii.load_rom(rom)
         nonlocal title
         title = rom.split("/")[-1].removesuffix(".ch8")
         logger.info(f"Loaded ROM-file {rom}")
-        reset_rom(instruction_interpreter, screen)
+        reset_rom()
 
     def set_cpu_rate(value, difficulty):
         print("Setting not implemented")
@@ -46,25 +47,26 @@ def main():
     sound = Sound()
 
     ii = InstructionInterpreter(screen, keyboard)
-    open_rom_file(ii, screen)
+    open_rom_file()
 
-    theme = pygame_menu.themes.Theme(background_color=(70,30,20),
-                                     title_background_color=(50,30,20),
+    theme = pygame_menu.themes.Theme(background_color=(70, 30, 20),
+                                     title_background_color=(50, 30, 20),
                                      widget_font=pygame_menu.font.FONT_DIGITAL,
                                      title_font_size=1,
                                      widget_font_size=14)
-    menu = pygame_menu.Menu(screen.MENU_HEIGHT, screen.DISPLAY.get_width(), '',
-                           theme=theme,
-                           joystick_enabled=False,
-                           keyboard_enabled=False,
-                           position=(0,0),
-                           columns=5,
-                           rows=1,
-                           mouse_motion_selection=True
-                           )
+    menu = pygame_menu.Menu(height=screen.MENU_HEIGHT, width=screen.DISPLAY.get_width(),
+                            title='',
+                            theme=theme,
+                            joystick_enabled=False,
+                            keyboard_enabled=False,
+                            position=(0, 0),
+                            columns=5,
+                            rows=1,
+                            mouse_motion_selection=True
+                            )
 
-    menu.add_button('Reset ROM', reset_rom, ii, screen)
-    menu.add_button('Load ROM', open_rom_file, ii, screen)
+    menu.add_button('Reset ROM', reset_rom)
+    menu.add_button('Load ROM', open_rom_file)
     menu.add.selector('CPU :', [('600Hz', 600),
                                 ('1200Hz', 1200),
                                 ('6000Hz', 6000)], onchange=set_cpu_rate)
@@ -72,9 +74,8 @@ def main():
     logger.info(f"Running main loop")
     clock = pygame.time.Clock()
     while True:
-        menu.mainloop(screen.DISPLAY, bgfun=None, clear_surface=False, disable_loop=True)
-        screen.spin()
         clock.tick(60)  # run at 60 fps
+        menu.mainloop(screen.DISPLAY, bgfun=None, clear_surface=False, disable_loop=True, fps_limit=0)
 
         if screen.paused:
             pygame.display.set_caption(f"{title}     PAUSED")
